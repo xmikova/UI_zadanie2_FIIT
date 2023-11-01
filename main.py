99ba0060beaa36a9e7f6ac22f3ea2ce3e99edf9d
Petra Mikova <petramikova10@gmail.com>
2023-11-01 09:50:19 +0100

    functional algo no code formatting no menu

--- main.py
+++ main.py
@@ -6,13 +6,13 @@ first_gen_mem_cells_count = 32
 max_steps = 500
 elitism = 0.02  # percent of new individuals made by elitism
 new_individuals = 0.15  # percent of new individuals made for new generation
-mutation_rate = 0.1  # probability of mutation
+mutation_rate = 0.05  # probability of mutation
 tournament = 0.2  # number of individuals in tournament
 
-number_of_gens = 5
-number_of_individuals = 14
+number_of_gens = 200
+number_of_individuals = 80
 
-#TODO : implementovat parser pre txt vstupu
+#TODO : implementovat parser pre txt vstupy
 
 class Individual:
     def __init__(self, path, memory_cells, treasures_found, fitness_func):
@@ -129,12 +129,11 @@ def found_treasures(individual):
 
         updated_path.append(move)
 
-        if len(individual.treasures_found) == len(all_treasures):
-            # All treasures found, searching ends
-            break  # Exit the loop
-        counter += 1
-
     individual.path = updated_path  # Update the path
+
+    if len(individual.treasures_found) == len(all_treasures):
+        # All treasures found, searching ends
+        return
 
 
 def calculate_fitness(individual):
@@ -152,88 +151,54 @@ def calculate_fitness(individual):
     fitness = (treasures_weight * treasures_found) + (steps_weight * total_steps)
 
     print(fitness)
+    if fitness == 49.999:
+        print(individual.path)
+        print(individual.treasures_found)
+
     return fitness
 
 
+def crossover(parent1, parent2):
+    # Perform single-point crossover
+    crossover_point = random.randint(1, mem_cells_count - 1)
 
-def roulette_selection(population, num_selected):
-    # Calculate the total fitness of the population
-    total_fitness = sum([ind.fitness_func for ind in individuals if isinstance(ind, Individual)])
+    child_memory_cells = parent1.memory_cells[:crossover_point] + parent2.memory_cells[crossover_point:]
 
-    selected = []
-    for _ in range(num_selected):
-        # Generate a random value between 0 and the total fitness
-        random_value = random.uniform(0, total_fitness)
-
-        current_sum = 0
-        for ind in population:
-            if isinstance(ind, Individual):
-                current_sum += ind.fitness_func
-                if current_sum >= random_value:
-                    selected.append(ind)
-                    break
-            else:
-                break
-
-    return selected
+    return child_memory_cells
 
 
-def roulette_selection_for_crossover(population, crossover_rate):
-    selected = []
+def roulette_selection(sorted_gen, new_generation):
+    start_index = int((elitism + new_individuals) * number_of_individuals)
+    weights = [ind.fitness_func for ind in sorted_gen]
 
-    # Sort the population by fitness in descending order
-    sorted_population = sorted(population, key=lambda ind: ind.fitness_func, reverse=True)
+    for i in range(start_index, number_of_individuals):
+        mom = random.choices(sorted_gen, weights=weights)[0]
+        dad = random.choices(sorted_gen, weights=weights)[0]
 
-    # Calculate the total fitness of the population
-    total_fitness = sum(ind.fitness_func for ind in sorted_population)
+        # Ensure that mom and dad are different individuals
+        while mom == dad:
+            dad = random.choices(sorted_gen, weights=weights)[0]
 
-    # Perform roulette wheel selection with a specified crossover rate
-    while len(selected) < len(population):
-        mom = roulette_selection(sorted_population, total_fitness)
-        dad = roulette_selection(sorted_population, total_fitness)
-
-        # Check if crossover should be performed based on crossover rate
-        if random.random() < crossover_rate:
-            offspring1, offspring2 = roulette_selection(mom, dad)
-            selected.extend([offspring1, offspring2])
-        else:
-            selected.extend([mom, dad])
-
-    return selected
+        # Create a new offspring Individual object and perform crossover on memory_cells
+        offspring_memory_cells = crossover(mom, dad)
+        offspring = Individual([], offspring_memory_cells, set(), 0)
+        new_generation[i] = offspring
 
 
-def tournament_selection(population, num_selected, tournament_size):
-    selected = []
+def tournament_selection(sorted_gen, new_generation):
+    start_index = int((elitism + new_individuals) * number_of_individuals)
+    num_tournament = int(tournament * number_of_individuals)
+    for i in range(start_index, number_of_individuals):
+        mom = sorted(random.choices(sorted_gen, k=num_tournament), reverse=True, key=lambda x: x.fitness_func)[0]
+        dad = sorted(random.choices(sorted_gen, k=num_tournament), reverse=True, key=lambda x: x.fitness_func)[0]
 
-    for _ in range(num_selected):
-        # Randomly select a subset of individuals for the tournament
-        tournament_subset = random.sample(population, tournament_size)
+        while numpy.array_equal(dad.memory_cells, mom.memory_cells):
+            dad = sorted(random.choices(sorted_gen, k=num_tournament), reverse=True, key=lambda x: x.fitness_func)[0]
 
-        # Choose the best individual from the tournament
-        best_individual = max(tournament_subset, key=lambda ind: ind.fitness_func)
-        selected.append(best_individual)
-
-    return selected
-
-
-def tournament_selection_for_crossover(population, tournament_size, crossover_rate):
-    selected = []
-
-    # Sort the population by fitness in descending order
-    sorted_population = sorted(population, key=lambda ind: ind.fitness_func, reverse=True)
-
-    # Perform tournament selection with a specified crossover rate
-    while len(selected) < len(population):
-        parents = [tournament_selection(sorted_population, tournament_size) for _ in range(2)]
-
-        # Check if crossover should be performed based on crossover rate
-        if random.random() < crossover_rate:
-            offspring1, offspring2 = tournament_selection(parents[0], parents[1])
-            selected.extend([offspring1, offspring2])
-        else:
-            selected.extend(parents)
-
-    return selected
+        # Create a new offspring Individual object and perform crossover on memory_cells
+        offspring_memory_cells = crossover(mom, dad)
+        offspring = Individual([], offspring_memory_cells, set(), 0)
+        new_generation[i] = offspring
 
 
 def single_point_crossover(parent1, parent2):
@@ -246,28 +211,6 @@ def single_point_crossover(parent1, parent2):
     return offspring
 
 
-def crossover_population(population, crossover_rate, tournament_size=None):
-    new_generation = []
-
-    # Calculate the number of individuals to apply crossover to based on crossover rate
-    num_to_crossover = int(crossover_rate * len(population))
-
-    for i in range(num_to_crossover):
-        if tournament_size is not None and random.random() < 0.5:
-            # Apply tournament selection with a 50% chance
-            parents = tournament_selection(population, 2, tournament_size)
-        else:
-            # Apply roulette selection with a 50% chance
-            parents = roulette_selection(population, 2)
-
-        # Perform single-point crossover on the selected parents
-        offspring1, offspring2 = single_point_crossover(parents[0], parents[1])
-
-        # Add the offspring to the new generation
-        new_generation.extend([offspring1, offspring2])
-
-    return new_generation
-
 def mutate_invert(individual):
     mutated_memory_cells = list(individual.memory_cells)  # Create a new list to hold the mutated memory cells
 
@@ -278,7 +221,6 @@ def mutate_invert(individual):
 
     mutated_individual = Individual(individual.path, mutated_memory_cells, individual.treasures_found, individual.fitness_func)
     return mutated_individual
-
 
 
 def mutation(new_generation):
@@ -303,10 +245,7 @@ def create_generation(sorted_gen):
     for k in range(start_fresh, end_fresh):
         new_gen[k] = spawn_individual()  # Create new individuals
 
-    roulette_selected = roulette_selection(sorted_gen, number_of_individuals - elite_end - end_fresh)
-
-    for m, ind in enumerate(roulette_selected, start=end_fresh):
-        new_gen[m] = ind  # Use the correct index here
+    roulette_selection(sorted_gen, new_gen)
 
     mutation(new_gen)  # Apply the "invert" mutation
 
@@ -317,39 +256,36 @@ def create_generation(sorted_gen):
 
 
 
-
 if __name__ == "__main__":
     # Initialize the population
     spawn_generation()
     best_individual = None
     sorted_individuals = {}
 
-    for length in range(number_of_gens):
+    for generation in range(number_of_gens):
+        # Calculate fitness for each individual in the population
+        for i in range(number_of_individuals-1):
+            virtual_machine(individuals[i])
+            found_treasures(individuals[i])
+            individuals[i].fitness_func = float(calculate_fitness(individuals[i]))
 
-        for generation in range(number_of_gens):
-            # Calculate fitness for each individual in the population
-            for i in range(number_of_individuals-1):
-                virtual_machine(individuals[i])
-                found_treasures(individuals[i])
-                individuals[i].fitness_func = calculate_fitness(individuals[i])
-                print("main ", individuals[i].fitness_func)
+        # Sort the population based on fitness in descending order
+        sorted_individuals = sorted([ind for ind in individuals if isinstance(ind, Individual)],
+                                    key=lambda ind: ind.fitness_func, reverse=True)
 
-            # Sort the population based on fitness in descending order
-            sorted_individuals = sorted([ind for ind in individuals if isinstance(ind, Individual)],
-                                        key=lambda ind: ind.fitness_func, reverse=True)
+        # Keep track of the best individual
 
-            # Keep track of the best individual
-            if best_individual is None or (len(sorted_individuals) != 0 and sorted_individuals[0].fitness_func > best_individual.fitness_func):
-                best_individual = sorted_individuals[0]
 
-            # Create a new generation
-            new_generation = create_generation(sorted_individuals)
+        # Create a new generation
+        new_generation = create_generation(sorted_individuals)
 
-            # Update the population with the new generation
-            individuals = new_generation  # Just replace the list with the new one
-            sorted_individuals = {}
+        # Update the population with the new generation
+        individuals = new_generation  # Just replace the list with the new one
+        sorted_individuals = {}
 
-        print("Best Individual - Fitness:", best_individual.fitness_func)
-        print("Path:", best_individual.path)
+
+
+    print("Best Individual - FINAL:", individuals[0].fitness_func)
+    print("Path:", individuals[0].path)
 
     # Print the best individual's path and fitness
